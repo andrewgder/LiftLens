c27a6ac0aa2569f536703177e1becbd15641ec7a
Andrew Der <andrew.g.der@gmail.com>
2024-09-25 21:23:37 -0400

    variable change

--- video-processing-service/src/storage.ts
+++ video-processing-service/src/storage.ts
@@ -4,8 +4,8 @@ import ffmpeg from "fluent-ffmpeg";
 
 const storage = new Storage();
 
-const rawVideoBucketName = "neetcode-yt-raw-videos";
-const processedVideoBucketName = "neetcode-yt-processed-videos";
+const rawVideoBucketName = "lift-lens-raw-videos";
+const processedVideoBucketName = "lift-lens-processed-videos";
 
 const localRawVideoPath = "./raw-videos";
 const localProcessedVideoPath = "./processed-videos";
