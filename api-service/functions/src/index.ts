a8cadaf7ed9f994c8742a13ca433807b342721b3
Andrew Der <andrew.g.der@gmail.com>
2024-10-08 13:36:41 -0400

    added getVideos function

--- api-service/functions/src/index.ts
+++ api-service/functions/src/index.ts
@@ -1,3 +1,4 @@
+// prettier-ignore
 import * as functions from "firebase-functions";
 
 // prettier-ignore
@@ -55,3 +56,23 @@ export const generateUploadUrl = onCall(
     return {url, fileName};
   }
 );
+
+const videoCollectionId = "videos";
+
+export interface Video {
+  id?: string;
+  uid?: string;
+  filename?: string;
+  status?: "processing" | "processed";
+  title?: string;
+  description?: string;
+}
+
+// prettier-ignore
+export const getVideos = onCall({maxInstances: 1}, async () => {
+  const querySnapshot = await firestore
+    .collection(videoCollectionId)
+    .limit(10)
+    .get();
+  return querySnapshot.docs.map((doc) => doc.data());
+});
