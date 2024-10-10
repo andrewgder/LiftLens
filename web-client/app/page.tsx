5bb7bc4b76bda94fffb1da375dd4e9efdb8153df
Andrew Der <andrew.g.der@gmail.com>
2024-10-10 13:26:34 -0400

    added videos to the UI and ability to watch

--- web-client/app/page.tsx
+++ web-client/app/page.tsx
@@ -1,16 +1,24 @@
 import styles from "./page.module.css";
+import Image from "next/image";
+import Link from "next/link";
+import { getVideos } from "./firebase/functions";
 
-export default function Home() {
+export default async function Home() {
+  const videos = await getVideos();
+
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <ol>
-          <li>
-            Get started by editing <code>app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-      </main>
-    </div>
+    <main>
+      {videos.map((video) => (
+        <Link href={`/watch?v=${video.filename}`} key={video.id}>
+          <Image
+            src={"/thumbnail.png"}
+            alt="video"
+            width={120}
+            height={80}
+            className={styles.thumbnail}
+          />
+        </Link>
+      ))}
+    </main>
   );
 }
